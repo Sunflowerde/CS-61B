19a886f9ea2dadb11910187f7a59008fa2fac534
ziwenxu <2410306105@stu.pku.edu.cn>
2025-01-18 11:39:59 +0800

    just do a little

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -5,8 +5,11 @@ public class JavaExercises {
 
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
-        // TODO: Fill in this function.
-        return null;
+        int [] dice = new int[6];
+        for (int i = 1; i <= 6; i++) {
+            dice[i-1] = i;
+        }
+        return dice;
     }
 
     /** Returns the order depending on the customer.
@@ -14,15 +17,29 @@ public class JavaExercises {
      *  If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
-        // TODO: Fill in this function.
-        return null;
+        if (customer.equals("Ergun")) {
+            return new String[] {"beyti", "pizza", "hamburger", "tea"};
+        } else if (customer.equals("Erik")) {
+            return new String[] {"sushi", "pasta", "avocado", "coffee"};
+        } else {
+            return new String[3];
+        }
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
-        // TODO: Fill in this function.
-        return 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            } else if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        int ans = max - min;
+        return ans;
     }
 
     /**
