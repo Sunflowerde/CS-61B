aa3e7c2b3a4566540a47de02d1fb0f2d87e4323e
ziwenxu <2410306105@stu.pku.edu.cn>
2025-01-23 23:17:24 +0800

    finished a part of proj1b

--- proj1b/src/ArrayDeque61B.java
+++ proj1b/src/ArrayDeque61B.java
@@ -9,13 +9,51 @@ public class ArrayDeque61B<T> implements Deque61B<T> {
     private int nextFirst;
     private int nextLast;
     private int size;
-    private final int INIT_CAPACITY = 8;
+    private int INIT_CAPACITY = 8;
 
     public ArrayDeque61B() {
         items = (T[]) new Object[INIT_CAPACITY];
         size = 0;
         nextFirst = 0;
         nextLast = 1;
+    }
+
+    private int nextOne(int index, int length) {
+        return Math.floorMod(index + 1, length);
+    }
+
+    private int minusOne(int index, int length) {
+        return Math.floorMod(index - 1, length);
+    }
+
+    private void resizeHelper(int capacity) {
+        T[] tmp = items;
+        int begin = nextOne(nextFirst, tmp.length);
+        int end = minusOne(nextLast, tmp.length);
+        items = (T[]) new Object[capacity];
+        for (int i = begin; i != end; i = nextOne(i, tmp.length)) {
+            items[nextLast] = tmp[i];
+            nextLast = nextOne(nextLast, capacity);
+        }
+        items[nextLast] = tmp[end];
+        nextLast = nextOne(nextLast, capacity);
+    }
+
+    private void expand() {
+        resizeHelper(items.length * 2);
+    }
+
+    private void shrink() {
+        resizeHelper(items.length / 2);
+    }
+
+    private void resize() {
+        if (size == items.length) {
+            expand();
+        }
+        if (size < items.length && items.length > 8) {
+            shrink();
+        }
     }
 
     @Override
@@ -38,7 +76,7 @@ public class ArrayDeque61B<T> implements Deque61B<T> {
     public List<T> toList() {
         List<T> returnList = new ArrayList<>();
         int begin = nextOne(nextFirst, items.length);
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i += 1) {
             returnList.add(items[begin]);
             begin = nextOne(begin, items.length);
         }
@@ -55,51 +93,12 @@ public class ArrayDeque61B<T> implements Deque61B<T> {
         return size;
     }
 
-    public int minusOne(int index, int length) {
-        return Math.floorMod(index - 1, length);
-    }
-
-    public int nextOne(int index, int length) {
-        return Math.floorMod(index + 1, length);
-    }
-
-    private void resize() {
-        if (size == items.length) {
-            expand();
-        } if (size < items.length / 4 && items.length > 8) {
-            shrink();
-        }
-    }
-
-    private void expand() {
-        resizeHelper(items.length * 2);
-    }
-
-    private void shrink() {
-        resizeHelper(items.length / 2);
-    }
-
-    private void resizeHelper(int capacity) {
-        T[] tmp = items;
-        int begin = nextOne(nextFirst, tmp.length);
-        int end = minusOne(nextLast, tmp.length);
-        items = (T[]) new Object[capacity];
-        nextFirst = 0;
-        nextLast = 1;
-        for (int i = begin; i != end; i = nextOne(i, tmp.length)) {
-            items[nextLast] = tmp[i];
-            nextLast = nextOne(nextLast, capacity);
-        }
-        items[nextLast] = tmp[end];
-        nextLast = nextOne(nextLast, capacity);
-    }
-
     @Override
     public T removeFirst() {
         resize();
-        int index = nextOne(nextFirst, items.length);
-        T removeItem = items[index];
-        items[index] = null;
+        int first = nextOne(nextFirst, items.length);
+        T removeItem = items[first];
+        items[first] = null;
         size -= 1;
         return removeItem;
     }
@@ -107,20 +106,21 @@ public class ArrayDeque61B<T> implements Deque61B<T> {
     @Override
     public T removeLast() {
         resize();
-        int index = minusOne(nextLast, items.length);
-        T removeItem = items[index];
-        items[index] = null;
+        int last = minusOne(nextLast, items.length);
+        T removeItem = items[last];
+        items[last] = null;
         size -= 1;
         return removeItem;
     }
 
     @Override
     public T get(int index) {
-        if (index < 0 || index >= size) {
+        if (index < 0 || index > items.length) {
             return null;
         }
-        index = Math.floorMod(nextOne(nextFirst, items.length) + index, items.length);
-        return items[index];
+        int first = nextOne(nextFirst, items.length);
+        int realIndex = Math.floorMod(first + index, items.length);
+        return items[realIndex];
     }
 
     @Override
